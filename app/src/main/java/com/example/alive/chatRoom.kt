78e4c99c6ede7385ad9633e951a4f1945ddb3b07
eogns47 <eogns47@naver.com>
2023-06-08 22:51:30 +0900

    video btn edit

--- app/src/main/java/com/example/alive/chatRoom.kt
+++ app/src/main/java/com/example/alive/chatRoom.kt
@@ -394,6 +394,7 @@ class chatRoom : AppCompatActivity() {
                 intent.putExtra("myVideoPath", data.videopath.toString())
 
                 startActivity(intent)
+                adapter.notifyDataSetChanged()
                 adapter.notifyItemChanged(position)
             }
         }
